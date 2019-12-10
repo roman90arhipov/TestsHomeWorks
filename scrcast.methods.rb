15f03d2dd94acbf2b2aeae66c80115beb5e3be05
Roman Arhipov <roman90arhipov@gmail.com>
2019-12-10 21:39:35 +0200

    локальные переменные в методе

---
+++ scrcast.methods.rb
@@ -0,0 +1,10 @@
+def meth1(name, weight)
+  puts "Why u so fat " + name + " " + weight
+end
+
+meth1("James", '(100kg)')
+puts "-----------------------------"
+meth1("Norton", '(120kg)')
+puts '-----------------------------'
+meth1("Jannet", '(150kg)')
+puts "WOW WOW...take it easy please"
